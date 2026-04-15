86011b495d347a6ff6f8adf56165c114743998b0
unknown <mohdharislcp@gmail.com>
2026-04-15 13:32:19 +0800

    Added for loop

--- main.cpp
+++ main.cpp
@@ -7,4 +7,12 @@ int main()
     cout <<"\n Hello Git " ;
     cout <<"\n I love C++" ;
 
+    for (int i=1; i<3 ; i++)
+    {
+        cout << "\n Hello git , this is line"   << i ;
+    }
+
+    return 0;
+
+
 }
